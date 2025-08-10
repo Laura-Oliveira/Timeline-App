2763577966658c16542468424b2cf3bedfa016d3
Laura-Oliveira <laura.regina.cp@gmail.com>
2025-08-10 15:25:33 -0300

    [Architecture][BackEnd] Improved the architecture by implemented Clean Architecture + Modularization + Feature-Oriented | Migrated Gradle from BuildConfig to Gradle Build Files |  Updated deprecated on build.gradle file  (module level) | Updated gradle version to 8.14.3 | Migrated deprecated Java code on SampleTimelineItens to up-to-date Kotlin code | Implemented the compose compiler in the correct way on libs.versions.toml file | Updated test lib versions on libs.versions.toml | Removed onstraintlayout from the libs.versons.toml file since it was not being used

---
+++ app/src/main/java/com/airtable/interview/airtableschedule/timeline/domain/TimelineUiState.kt
@@ -0,0 +1,10 @@
+package com.airtable.interview.airtableschedule.timeline.domain
+
+import com.airtable.interview.airtableschedule.timeline.data.Event
+
+/**
+ * UI state for the timeline screen.
+ */
+data class TimelineUiState(
+    val events: List<Event> = emptyList(),
+)
---
+++ app/src/main/java/com/airtable/interview/airtableschedule/timeline/domain/TimelineViewModel.kt
@@ -0,0 +1,24 @@
+package com.airtable.interview.airtableschedule.timeline.domain
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+/**
+ * ViewModel responsible for managing the state of the timeline screen.
+ */
+class TimelineViewModel: ViewModel() {
+    private val eventDataRepository: EventDataRepository = EventDataRepositoryImpl()
+
+    val uiState: StateFlow<TimelineUiState> = eventDataRepository
+        .getTimelineItems()
+        .map(::TimelineUiState)
+        .stateIn(
+            viewModelScope,
+            initialValue = TimelineUiState(),
+            started = SharingStarted.Companion.WhileSubscribed()
+        )
+}
