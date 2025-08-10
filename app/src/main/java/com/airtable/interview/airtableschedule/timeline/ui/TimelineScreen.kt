23903b971afb6f0db5b7550717f31ecaa2f2a16c
Laura-Oliveira <laura.regina.cp@gmail.com>
2025-08-10 16:59:22 -0300

    [FrontEnd] Implemented the new theme on the timeline events and created a new logic to arrange the dates below the titles

--- app/src/main/java/com/airtable/interview/airtableschedule/timeline/ui/TimelineScreen.kt
+++ app/src/main/java/com/airtable/interview/airtableschedule/timeline/ui/TimelineScreen.kt
@@ -1,17 +1,27 @@
 package com.airtable.interview.airtableschedule.timeline.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.airtable.interview.airtableschedule.timeline.data.Event
 import com.airtable.interview.airtableschedule.timeline.domain.TimelineViewModel
+import com.airtable.interview.airtableschedule.timeline.ui.theme.AirtableScheduleTheme
 
 /**
  * A screen that displays a timeline of events.
@@ -19,10 +29,13 @@ import com.airtable.interview.airtableschedule.timeline.domain.TimelineViewModel
 @Composable
 fun TimelineScreen(
     viewModel: TimelineViewModel = viewModel()
-) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-    TimelineView(uiState.events)
+)
+{
+    AirtableScheduleTheme {
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+        TimelineView(uiState.events)
+        TimelineView(uiState.events)
+    }
 }
 
 /**
@@ -34,8 +47,18 @@ fun TimelineScreen(
 @Composable
 private fun TimelineView(
     events: List<Event>,
-) {
-    Column(modifier = Modifier.fillMaxWidth()) {
+)
+{
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    )
+    {
         events.forEach {
             EventView(event = it)
         }
@@ -47,10 +70,50 @@ private fun TimelineView(
  * TODO: This needs to be updated as needed
  */
 @Composable
-private fun EventView(event: Event) {
-    Column(verticalArrangement = Arrangement.SpaceBetween) {
-        Text(text = event.name)
-        Text(text = event.startDate.toString())
-        HorizontalDivider()
+private fun EventView(event: Event)
+{
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        shape = RoundedCornerShape(12.dp),
+        tonalElevation = 2.dp,
+        shadowElevation = 1.dp,
+        modifier = Modifier.fillMaxWidth()
+    )
+    {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+        )
+        {
+            Text(
+                text = event.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Row (modifier = Modifier.fillMaxWidth())
+            {
+                Text(
+                    text = "(${event.getFormattedStartDate()} - ",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primaryContainer
+
+                )
+                if (event.startDate != event.endDate)
+                {
+                    Text(
+                        text = "${event.getFormattedEndDate()})",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.primaryContainer
+                    )
+                }
+            }
+
+            HorizontalDivider(
+                color = MaterialTheme.colorScheme.secondary,
+                thickness = 1.dp
+            )
+        }
     }
 }
